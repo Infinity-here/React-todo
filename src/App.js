23377a2e7f033e3961ba5dedaa8ed2b467f3f75e
infinity-here <anant161100@gmail.com>
2023-08-10 15:28:32 +0530

    first react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Todolist from "./component/Todolist";
+import Tododone from "./component/tododone";
+import Todonotdone from "./component/todonotdonelist";
+import { SharedArrayProvider } from "./context/context";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <div className="tasklist">
+        <SharedArrayProvider>
+          <Tododone />
+          <Todolist />
+          <Todonotdone />
+        </SharedArrayProvider>
+      </div>
     </div>
   );
 }
